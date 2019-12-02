4ddf963d2c07546b7fbc28dc6cbbcc04159e5abf
Dheeraj <kontham.dheeraj@gmail.com>
2019-12-02 13:12:34 +0530

    Updated GlobalExceptionHandler class annotationt to RestControllerAdvice

--- src/main/java/com/projects/springboothystrix/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/projects/springboothystrix/global/exception/handler/GlobalExceptionHandler.java
@@ -3,13 +3,14 @@ package com.projects.springboothystrix.global.exception.handler;
 import java.util.Date;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
 import com.projects.springboothystrix.exception.StudentException;
 
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler{
 
 	@ExceptionHandler(Exception.class)
